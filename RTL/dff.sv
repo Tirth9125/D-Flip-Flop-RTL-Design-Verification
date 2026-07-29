5ed4d51ac367ada0f07f2e910669e0e9114c0163
Tirth9125 <bavaliyatirth9125@gmail.com>
2026-07-29 12:05:01 +0530

    Update dff.sv

--- RTL/dff.sv
+++ RTL/dff.sv
@@ -17,11 +17,3 @@ module dff (dff_if vif);
   
 endmodule
  
-// Define an interface "dff_if" with the following signals
-interface dff_if;
-  logic clk;   // Clock signal
-  logic rst;   // Reset signal
-  logic din;   // Data input
-  logic dout;  // Data output
-  
-endinterface
